b545e4e0ebf9e2fb34ebc81ed7ff402044a05121
Théo M <theopublic@neuf.fr>
2018-04-11 11:06:28 +0200

    Base app test

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+
+
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + 'public/index.html');
+});
+
+var server = app.listen(80, console.log('Server started on port 80'));
