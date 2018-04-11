1e176c8eed0270f70653e0205c1614ca3bd30d9e
Thooto <theopublic@neuf.fr>
2018-04-11 11:27:59 +0200

    dzq

--- app.js
+++ app.js
@@ -11,4 +11,4 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + 'public/index.html');
 });
 
-var server = app.listen(80, console.log('Server started on port 80'));
+var server = app.listen(process.env.PORT || 80, console.log('Server started on port 80'));
